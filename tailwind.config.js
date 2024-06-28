ae9069c124a09692d1d3030d9dd3e2489f8a3004
1sfandyor <rapbrand06112000@gmail.com>
2024-06-28 05:49:41 -0700

    Add more screns

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,26 @@ export default {
   ],
   theme: {
     extend: {
+      content: {
+        'karzinka': 'url("./assets/karzinka.svg")',
+        'arrowUpIcon': 'url("../src/arrow-up-icon.svg")',
+      },
       screens: {
-        sm: '476px',
-        lt: '603px',
-        smd: '617px',
+        xs: '500px',
+        sm: '601px',
+        lt: '617px',
+        xlt: '674px',
+        smd: '689px',
+        lmd: '748px',
         md: '768px',
+        mmd: '829px',
+        xmd: '887px',
+        xslp: '955px',
+        slp: '993px',
+        llp: '1006px',
         lp: '1024px',
+        mlp: '1119px',
+        xlp: '1214px',
         lg: '1300px',
         xl: '1440px',
       '2xl': '1990px',
@@ -43,7 +57,11 @@ export default {
         yellow: "#FFF4A3",
         pink: '#ffc0c7',
         cyan: '#96D4D4',
+        
+        // PURPLE
         purple: "#c5a5c5",
+        darkPurple: '#9763f6',
+
         brown: "#a52a2a"
       },
     },
